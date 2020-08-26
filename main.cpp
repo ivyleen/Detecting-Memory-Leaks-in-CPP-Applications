78d19709fe05875b933fc94c7dcfe155c01dfa5b
Ivy Ivanova <ivaivanova091@gmail.com>
2020-08-26 10:25:44 +0300

    [code review] refactoring changes

--- main.cpp
+++ main.cpp
@@ -4,9 +4,6 @@
 #include <climits>
 #include <thread>
 
-int g_value; // global value ----> stored in data segment
-	     // of the virtual address space
-
 int* a_ptr[5]{};
 
 // handler if operator new functions fail
@@ -22,22 +19,6 @@ void NewHandler()
 
 int main()
 {
-	// all variables here are stored on the stack
-	int value {}; 		 
-	
-	int *p = & value;	
-	
-	int *p1 = new int {};	// this memory is allocated on the heap
-				// and p1 is pointing to the memory that 
-				// is returned by the new expression
-
-	int *p2 = & g_value;	// this pointer points to the data segment
-
-	int (*pFn)() = main;	// this pointer points to the code segment
-				// of the virtual address space because 
-				// functions are stored in there
-
-
 	// Dynamic allocation 
 	// NOTE: dynamic memory can be accessed throughout the program
 	// as lon as you have the address. But the lifetime of the memory
