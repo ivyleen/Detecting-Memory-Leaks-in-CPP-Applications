65fd04aeab1f5c07e59535bc518dc5a293279233
Ivy Ivanova <ivaivanova091@gmail.com>
2020-08-27 09:02:32 +0300

    [placement new] how placement new works

--- placement_new.cpp
+++ placement_new.cpp
@@ -26,17 +26,18 @@ public:
 
 int main()
 {
+	const int SIZE = 5;
 	// if we know what are the actual values we can use uniform initialization 
 	// (C++11 needed) in order to avoid the temporary objects and unnecessary calls
 	// to the default constructor as well as asignment
-	Reporter r[5]{1,2,3,4,5};
+	//Reporter r[SIZE]{1,2,3,4,5};
 
 	// if we want to create the Reporter objects on the heap
 	// we see that only the constructor is called and not the destructor
-	//Reporter *r1 = new Reporter [5]; 
+	//Reporter *r1 = new Reporter [SIZE]; 
 	
 	// if we want to initialize the values inside of r1 we need to do a loop
-	/*for (int i = 0 ; i < 5; ++i)
+	/*for (int i = 0 ; i < SIZE; ++i)
 	{
 		// this line of code will cause in creating a temporary object
 		// constructed with the parameterized constructor; then this temporary will be
@@ -47,9 +48,35 @@ int main()
 
 	// so in order to avoid the asignment and we know the values we can use
 	// uniform initialization with new expressions
-	Reporter *r2 = new Reporter [5] {1,2,3,4,5};
+	//Reporter *r2 = new Reporter [SIZE] {1,2,3,4,5};
 
 	// we need to manually delete
 	//delete[] r1;
-	delete[] r2;
+	//delete[] r2;
+
+
+	// if we don't know the values and we still it on the heap without any asignments
+	// we need to invoke operator new function on our own
+	Reporter *r3 = static_cast<Reporter*>(operator new (SIZE * sizeof(Reporter))) ;
+
+	for (int i = 0; i < SIZE; ++i)
+	{
+		// (r3 + i) --> this calculation is needed so we can iterate over the 
+		// allocated memory and then put the Reporter objects in there
+		// the address will increase with the size of the object
+		new(r3 + i) Reporter{i};
+	}
+
+	// calling placement new we need to invoke a destructor also
+	// but calling delete keyword on the r3 pointer will not call the destructors of 
+	// the Reporter class; the reason we use operator new function to allocate
+	// so we need manually to invoke the destructors of every object in the array
+	// and the delete the pointer
+	for (int i = 0; i < SIZE; ++i)
+	{
+		r3->~Reporter();
+	}
+	operator delete (r3);	
+
+
 }
